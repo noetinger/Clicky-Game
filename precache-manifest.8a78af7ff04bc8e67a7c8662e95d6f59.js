5b614d973e79118bf5eaed81db6b0a1a8b863cc4
Nick O <nicholas.oetinger@gmail.com>
2019-09-05 18:12:12 -0400

    Updates

---
+++ precache-manifest.8a78af7ff04bc8e67a7c8662e95d6f59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac6fa09b84d2f5ed0b7ce2bcaf67a37",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "795f86c3a7521da2f496",
+    "url": "/Clicky-Game/static/css/main.3094112a.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/Clicky-Game/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "795f86c3a7521da2f496",
+    "url": "/Clicky-Game/static/js/main.c5b0aa14.chunk.js"
+  },
+  {
+    "revision": "75fa83e2a97bc1c69d72",
+    "url": "/Clicky-Game/static/js/runtime~main.4267543b.js"
+  }
+]);
