a894920a436fbc360df36783a88023ed85c3fa9f
Nick O <nicholas.oetinger@gmail.com>
2019-09-05 18:04:51 -0400

    Moved all the files out of sub-folder

---
+++ build/precache-manifest.263889944c728f2901d052e2ab00ea02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9c65e422351be3a19b54d7ddecf071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d25dd67bf5b7ef02ee",
+    "url": "/static/css/main.3094112a.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "62d25dd67bf5b7ef02ee",
+    "url": "/static/js/main.8bd87d1a.chunk.js"
+  },
+  {
+    "revision": "0ebfaf63642c8e34abd7",
+    "url": "/static/js/runtime~main.b027e18f.js"
+  }
+]);
